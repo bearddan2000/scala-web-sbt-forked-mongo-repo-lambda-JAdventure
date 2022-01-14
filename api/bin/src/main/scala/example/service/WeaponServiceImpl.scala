99eee745cc97ad1de0241977ddb669637fb5c289
bearddan2000 <bearddan2000@yahoo.com>
2022-01-13 19:03:00 -0500

    initial commit

---
+++ api/bin/src/main/scala/example/service/WeaponServiceImpl.scala
@@ -0,0 +1,25 @@
+package example.service;
+
+import example.model.Weapon;
+import example.repository.WeaponRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import scala.collection.JavaConversions._
+
+/**
+ * @author ragcrix
+ */
+@Service
+class WeaponServiceImpl @Autowired() (
+  weaponRepository: WeaponRepository
+) extends WeaponService {
+
+    override def findAll(): List[Weapon] = weaponRepository.findAll()
+
+    override def findByName(name: String): Weapon = weaponRepository.findAll()
+    .filter(x => x.name == name)
+    .head
+}
